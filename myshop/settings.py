2e55818ca1aae184a2661e2798ce21ae2e2e93b1
smoke <mohamedmura1995@gmail.com>
2022-05-29 18:23:16 +0200

    'last'

--- myshop/settings.py
+++ myshop/settings.py
@@ -93,12 +93,8 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database1',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
