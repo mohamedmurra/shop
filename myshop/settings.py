2c3559dc96fe88bbddbd14889ed97efa701883f0
smoke <mohamedmura1995@gmail.com>
2022-07-22 14:15:51 +0200

    'heroku'

--- myshop/settings.py
+++ myshop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'Blog',
     'crispy_forms',
     'crispy_bootstrap5',
+    'gunicorn'
 
 ]
 
@@ -101,7 +102,7 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': 'pEEZRpZwsg2nvbPPoaax',
