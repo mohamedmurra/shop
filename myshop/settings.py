9c8e00c908e59d43c63baab7f3e580300ad4ead7
smoke <mohamedmura1995@gmail.com>
2022-07-22 10:40:51 +0200

    'lasaf'

--- myshop/settings.py
+++ myshop/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'pEEZRpZwsg2nvbPPoaax',
+        'HOST': 'containers-us-west-73.railway.app',
+        'PORT': '5480',
     }
 }
 
